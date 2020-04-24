bc64500b4ee9f91d3f45426d0db93807d662f76c
SpukiBugi <Jaguar757@hotmail.com>
2020-04-25 01:14:53 +0300

    ui elems

---
+++ Assets/Scripts/MenuEvents.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        onEnable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Menu Enabling
+    void onEnable() {
+        var a = AssetDatabase.LoadAssetAtPath( "Assets/hierarchy.uxml"); 
+        VisualElement row = a.CloneTree(); 
+
+        var label = row.Query(className: "test");
+        label.RegisterCallback<MouseUpEvent>( evt => evt.StopPropagation()); 
+
+        rootVisualElement.Add(row);
+    }
+}
